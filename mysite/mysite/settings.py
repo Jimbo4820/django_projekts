0bde1952d38b63699ec4574963907e40290a4f54
Jimbo <jimbo.oil.4820@gmail.com>
2023-10-10 15:53:59 +0200

    update_commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# configuration server for email
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dima.daster.5355@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
