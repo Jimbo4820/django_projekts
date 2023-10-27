2cff230bca7538131c7b7fdfce17b606ee9b0bb4
Jimbo <jimbo.oil.4820@gmail.com>
2023-10-27 13:44:25 +0200

    add_mail_authenticate_commit

--- my_test_soc_site/bookmarks/account/views.py
+++ my_test_soc_site/bookmarks/account/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+from datetime import datetime
 
 
 def user_login(request):
@@ -55,6 +57,7 @@ def dashboard(request):
 
 @login_required
 def edit(request):
+    now = datetime.now()
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user, data=request.POST)
         profile_form = ProfileEditForm(instance=request.user.profile,
@@ -63,6 +66,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
